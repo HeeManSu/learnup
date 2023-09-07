6ca6b41120d8ea71728ac014560639c5f6724b10
Himanshu <himanshu.kumarr07@gmail.com>
2023-09-07 11:47:08 +0530

    Bug fixed ✨

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,7 @@ import { courseReducer } from "./reducers/courseReducer";
 import { adminReducer } from "./reducers/adminReducer";
 import { otherReducer } from "./reducers/otherReducer";
 
-export const server = "http://localhost:4002/api/v1";
+export const server = "http://localhost:4004/api/v1";
 const store = configureStore({
     reducer: {
         user: userReducer,
